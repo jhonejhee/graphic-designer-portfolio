2d81bd59c16e435c3b3c9d39becf83963f5f96b2
jhonejhee <jhonejheepatacsil@gmail.com>
2025-04-05 03:33:56 +0800

    added layouts and homepage

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,6 @@ function NavBar() {
     <nav class="bg-transparent border-gray-200 h-16">
         <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4 py-12">
             <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                {/* <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" /> */}
                 <span class="self-center text-xl font-semibold whitespace-nowrap text-white font-normal">🟢 available for <span  className="text-accent">work</span> </span>
             </a>
             <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
