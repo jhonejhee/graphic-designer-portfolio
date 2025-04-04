6b704efe17d70adf878e2993bf2a324f1dc30b2c
jhonejhee <jhonejheepatacsil@gmail.com>
2025-04-05 03:42:07 +0800

    fixed font sizes and weights

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,9 @@
 function NavBar() {
   return (
     <nav class="bg-transparent border-gray-200 h-16">
-        <div class="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4 py-12">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 py-12">
             <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <span class="self-center text-xl font-semibold whitespace-nowrap text-white font-normal">🟢 available for <span  className="text-accent">work</span> </span>
+                <span class="self-center text-lg font-normal whitespace-nowrap text-white">🟢 available for <span  className="text-accent">work</span> </span>
             </a>
             <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -15,19 +15,19 @@ function NavBar() {
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium text-xl flex flex-col p-4 md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 ">
                     <li>
-                        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 underline" aria-current="page">FACEBOOK</a>
+                        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 underline text-lg font-normal" aria-current="page">FACEBOOK</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 underline" aria-current="page">INSTAGRAM</a>
+                        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 underline text-lg font-normal" aria-current="page">INSTAGRAM</a>
                     </li>
                     <li>
-                        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 underline" aria-current="page">LINKEDIN</a>
+                        <a href="#" class="block py-2 px-3 text-white rounded-sm md:bg-transparent md:p-0 underline text-lg font-normal" aria-current="page">LINKEDIN</a>
                     </li>
                 </ul>
             </div>
             <div class="hidden w-full lg:block md:w-auto">
                 <a href="mailto:jiannina.ayon@gmail.com" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <span class="self-center text-xl font-semibold whitespace-nowrap text-white font-normal">JIANNINA.AYON@GMAIL.COM</span>
+                    <span class="self-center text-lg font-normal whitespace-nowrap text-white font-normal">JIANNINA.AYON@GMAIL.COM</span>
                 </a>
             </div>
         </div>
