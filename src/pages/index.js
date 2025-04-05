6c509ce5015ea0c857f3a29f07dbb90b4741cb9f
jhonejhee <jhonejheepatacsil@gmail.com>
2025-04-05 11:12:28 +0800

    removed paging

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,7 @@
-import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
-import { ChevronRight, ChevronLeft } from "lucide-react";
-
-const pages = [
-  { name: "Home", path: "/home" },
-  { name: "About", path: "/about" },
-  { name: "Projects", path: "/projects" },
-  { name: "Contact", path: "/contact" },
-]
 
 function Portfolio() {
-  const [index, setIndex] = useState(0);
-  const [path, setPath] = useState(pages[index].path);
-
-  useEffect(() => {
-    setPath(pages[index].path);
-  }, [index]);
-
-  useEffect(() => {
-    console.log(path);
-  }, [path]);
-
   return (
     <div
         className="h-screen w-full overflow-hidden font-poppins select-none"
@@ -37,20 +17,6 @@ function Portfolio() {
         
         <main className="h-full w-full relative">
           <Outlet />
-
-          {/* Left Button */}
-          <div className="fixed top-1/2 left-0 -translate-y-1/2 z-50">
-            <button className="text-white p-2 hover:text-accent" onClick={() => setIndex((index - 1 + pages.length) % pages.length)}>
-              <ChevronLeft className="h-10 w-10" />
-            </button>
-          </div>
-
-          {/* Right Button */}
-          <div className="fixed top-1/2 right-0 -translate-y-1/2 z-50">
-            <button className="text-white p-2 hover:text-accent" onClick={() => setIndex((index + 1) % pages.length)}>
-              <ChevronRight className="h-10 w-10" />
-            </button>
-          </div>
         </main>
 
         
