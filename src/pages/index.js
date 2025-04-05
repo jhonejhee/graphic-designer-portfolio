181a1dd508364b17ca8d17252e4a574fe979c6bd
jhonejhee <jhonejheepatacsil@gmail.com>
2025-04-05 11:51:49 +0800

    added sections by components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,19 @@
 import NavBar from "../components/NavBar";
 import { Outlet } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+const sections = [
+  { name: "Home", href: "#home", section: <Home /> },
+  { name: "About", href: "#about", section: <About /> },
+  // { name: "Portfolio", href: "#portfolio" },
+  // { name: "Contact", href: "#contact" },
+]
 
 function Portfolio() {
   return (
     <div
-        className="h-screen w-full overflow-hidden font-poppins select-none flex flex-col"
+        className="h-fit w-full overflow-auto font-poppins select-none flex flex-col items-center justify-center"
         style={{
           backgroundImage: "url('/images/background-landscape.png')",
           backgroundSize: 'cover',
@@ -13,13 +22,19 @@ function Portfolio() {
           backgroundAttachment: 'fixed',
         }}
     >
+      <div className="h-fit w-full flex flex-col items-center justify-center  max-w-screen-xl">
+        {/* Nav Bar */}
         <NavBar/>
-        
-        <main className="h-full w-full">
-          <Outlet />
-        </main>
 
-        
+        {/* Sections */}
+        <main className="h-full w-full">
+          {sections.map((section) => (
+            <div key={section.name} id={section.href.slice(1)} className="h-screen w-full flex items-center justify-center">
+              {section.section}
+            </div>
+          ))}
+        </main>
+      </div>
     </div>
   );
 } 
