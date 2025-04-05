02b3999d230d0f75f4ec756bab9cf0ca2a93807c
jhonejhee <jhonejheepatacsil@gmail.com>
2025-04-05 13:15:57 +0800

    added about page

---
+++ src/pages/About/index.js
@@ -0,0 +1,36 @@
+
+function About() {
+    return (
+      <div
+          className="flex items-center justify-between h-full w-full p-8"
+      >
+          <div className="flex flex-col gap-8 items-start justify-center h-full w-full text-start w-3/4">
+            <span className="text-md text-beige tracking-[0.5rem]">
+                ABOUT ME&nbsp;
+                <span className="italic">(JA-NI-NA)</span>
+            </span>
+            <span className="text-5xl md:text-7xl normal-case text-beige tracking-normal">
+                I'm&nbsp;
+                <span className="text-accent">Jiannina</span>
+                &nbsp;- where creativity and caffeine collide.
+            </span>
+            <span className="text-md text-accent tracking-[0.5rem] whitespace-pre-wrap">
+                BASED IN PHILIPPINES
+            </span>
+            <p className="text-md text-beige tracking-normal whitespace-pre-wrap">
+            I love bringing ideas to life—whether it's through design, video, or content that connects with people.
+            Keeping things organized gives me a weird sense of satisfaction (in the best way possible), and I enjoy making
+            sure everything runs smoothly, from social media pages to technical setups during trainings. And I'm still
+            learning and growing every day, but that's what makes the journey exciting!
+            <br/>
+            <br/>
+            When I'm not working on content, I'm probably lost in a game or dancing like nobody's watching.
+            </p>
+          </div>
+  
+      </div>
+    );
+  }
+  
+  export default About;
+  
