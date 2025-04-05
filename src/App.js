86284cce45afca0dc6668cd2e4be83088a1ecfdd
jhonejhee <jhonejheepatacsil@gmail.com>
2025-04-05 11:51:34 +0800

    removed routing

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Portfolio />}>
-          {/* Nested routes */}
-          <Route index element={<Home />} />
-          {/* <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} /> */}
-        </Route>
-      </Routes>
-    </Router>
+    <Portfolio/>
   );
 }
 
